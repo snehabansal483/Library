e58e9bff0e88f465fb8be6dffd6467bcc473bb6f
chaman sinha <chamansinha9480@gmail.com>
2026-01-04 17:11:58 +0530

    exteral port added

--- library_web/db_config.py
+++ library_web/db_config.py
@@ -9,6 +9,7 @@ load_dotenv()
 def connect_db():
     return mysql.connector.connect(
         host=os.getenv('MYSQL_HOST', 'localhost'),
+        port=int(os.getenv('MYSQL_PORT', '3306')),
         user=os.getenv('MYSQL_USER', 'root'),
         password=os.getenv('MYSQL_PASSWORD'),
         database=os.getenv('MYSQL_DATABASE', 'library_db')
